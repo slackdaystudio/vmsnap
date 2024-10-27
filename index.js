9268a9d8ed565d2d2bb26cf30f30bbaec4b0a2c4
Phil Guinchard <phil.guinchard@gmail.com>
2024-10-27 18:49:42 -0400

    Configuring husky

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 #!/usr/bin/env node
 /**
- * This script is designed to backup KVM virtual machines using the 
+ * This script is designed to backup KVM virtual machines using the
  * virtnbdbackup utility.  It relies on the virsh and qemu-img utilities as well
  * so please make sure you have them installed.
- * 
+ *
  * Usage: node index.js --domains=<domain> --output=<output>
- * 
+ *
  * --domains: Comma-separated list of domains to backup.  Use '*' to backup all
  * domains.
  * --output: Output directory for the backups.  Only supply the root of where
- * you want the backups to go.  The script will create a directory for each 
+ * you want the backups to go.  The script will create a directory for each
  * domain and a subdirectory for each year/month.
- * 
+ *
  * @author: Philip J. Guinchard <phil.guinchard@gmail.com>
  */
 
@@ -51,7 +51,7 @@ const checkDependencies = async () => {
 
 /**
  * Returns the name of the backup folder for the current month.
- * 
+ *
  * @returns {string} Current date in the format YYYY-MM
  */
 const getBackupFolder = () => {
@@ -60,18 +60,18 @@ const getBackupFolder = () => {
 
 /**
  * Check if a domain exists on the host system.
- * 
+ *
  * @param {string} domain the name of the domain
  * @returns {Promise<boolean>} True if domain exists, false otherwise
  */
 const domainExists = async (domain) => {
-    // Check if domain name contains invalid characters
-    // https://bugs.launchpad.net/ubuntu/+source/libvirt/+bug/672948
-    if (/^[A-Za-z0-9_\.\+\-&:/]*$/.test(domain) === false) {
-        console.error(`Domain ${domain} contains invalid characters`);
+  // Check if domain name contains invalid characters
+  // https://bugs.launchpad.net/ubuntu/+source/libvirt/+bug/672948
+  if (/^[A-Za-z0-9_\.\+\-&:/]*$/.test(domain) === false) {
+    console.error(`Domain ${domain} contains invalid characters`);
 
-        return false;
-    }
+    return false;
+  }
 
   const command = [VIRSH, 'domstate', domain];
 
@@ -86,7 +86,7 @@ const domainExists = async (domain) => {
 
 /**
  * List all domains on the host system.
- * 
+ *
  * @returns {Promise<Array<string>>} List of all domains
  */
 const fetchAllDomains = async () => {
@@ -103,7 +103,7 @@ const fetchAllDomains = async () => {
 
 /**
  * Inspects the --domains CLI argument and returns a list of domains to backup.
- * 
+ *
  * @returns {Promise<Array<string>>} List of domains to backup
  */
 const parseDomains = async () => {
@@ -123,8 +123,8 @@ const parseDomains = async () => {
 };
 
 /**
- * Perform a backup of a domain, runnig or stopped.
- * 
+ * Perform a backup of a domain, running or stopped.
+ *
  * @param {Promise<string>} domain the domain to backup
  */
 const backup = async (domain) => {
